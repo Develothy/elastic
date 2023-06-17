57db4168b72bd9cdf49a06879768e3a57eb3ada2
rothy <rothy@lezhin.com>
2023-06-17 23:50:55 +0900

    bulk 처리

--- src/main/java/com/rothy/elastic/controller/ESController.java
+++ src/main/java/com/rothy/elastic/controller/ESController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,22 +15,27 @@ public class ESController {
     private ESService esService;
 
 
-    @PostMapping("/create/{indexName}")
-    public @ResponseBody String createIndexWithNori(@PathVariable String indexName) throws IOException {
+    @PostMapping("/{indexName}")
+    public @ResponseBody String createIndex (@PathVariable String indexName) throws IOException {
 
         return esService.createIndexWithNori(indexName);
     }
 
     @GetMapping("/search")
-    public @ResponseBody String getSearchResult(@RequestParam String field, @RequestParam String q) throws IOException {
+    public @ResponseBody String search (@RequestParam String field, @RequestParam String q) throws IOException {
 
         return esService.getSearchResult(field, q);
     }
 
-    @PostMapping("/insert/{id}")
-    public @ResponseBody String getInsertResult(@PathVariable Long id, @RequestBody Map<String, Object> source) throws IOException {
+    @PostMapping("/{indexName}/{id}")
+    public @ResponseBody String add (@PathVariable String indexName, @PathVariable Long id, @RequestBody Map<String, Object> source) throws IOException {
 
-        return esService.insertDocument(id, source);
+        return esService.insertDocument(indexName, id, source);
+    }
+    @PostMapping("/{indexName}/bulk")
+    public @ResponseBody String bulk (@PathVariable String indexName, @RequestBody List<Map<String, Object>> source) throws IOException {
+
+        return esService.insertBulk(indexName, source);
     }
 
 }
--- src/main/java/com/rothy/elastic/service/ESService.java
+++ src/main/java/com/rothy/elastic/service/ESService.java
@@ -1,9 +1,14 @@
 package com.rothy.elastic.service;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
@@ -15,17 +20,20 @@ import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 
 @Service
 public class ESService {
     @Autowired
-    private RestHighLevelClient restHighLevelClient;
+    private RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+            RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
     private static final String MY_INDEX = "my_index";
     private static final String DOC_TYPE = "_doc";
@@ -80,7 +88,7 @@ public class ESService {
     }
 
 
-    public String insertDocument(Long id, Map<String, Object> source) throws IOException {
+    public String insertDocument(String indexName, Long id, Map<String, Object> source) throws IOException {
 
         IndexRequest indexRequest = new IndexRequest(MY_INDEX)
                 .id(String.valueOf(id))
@@ -92,5 +100,19 @@ public class ESService {
         return response.toString();
     }
 
+    public String insertBulk (String indexName, List<Map<String, Object>> sources) throws IOException {
+
+        BulkRequest bulkReq = new BulkRequest();
+
+        for (Map<String, Object> source : sources) {
+            IndexRequest indexReq = new IndexRequest(indexName)
+                    .source(source, XContentType.JSON);
+            bulkReq.add(indexReq);
+        }
+
+        BulkResponse response = restHighLevelClient.bulk(bulkReq, RequestOptions.DEFAULT);
+
+        return response.toString();
+    }
 
 }
